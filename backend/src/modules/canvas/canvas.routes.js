348c51e540848e79ff62e831782e68f4b4b38ddd
Abir Maity <abirmaity44@gmail.com>
2025-11-12 20:32:51 +0530

    start from new just auth

--- backend/src/modules/canvas/canvas.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("./canvas.controller");
-
-router.get("/:projectId", controller.getCanvas);
-router.post("/:projectId", controller.saveCanvas);
-
-module.exports = router;
